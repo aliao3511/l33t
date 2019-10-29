f529aa57942a802a67370029287437aeec77e11e
Alex Liao <alexandraliao3511@gmail.com>
2019-10-29 19:28:18 -0400

    complete lc3 js

--- 3.js
+++ 3.js
@@ -1,19 +1,14 @@
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
     let start = 0;
     let max = 0;
     let chars = {};
     for (let end = 0; end < s.length; end++) {
-        let current = s[end]
+        let current = s[end];
         if (chars[current] !== undefined) {
-            end = Math.max(chars[current] + 1, start + 1);
-	    max = Math.max(end - start, max);
-	    chars = {};
-	}
+            start = Math.max(chars[current] + 1, start);
+        }
+        max = Math.max(end - start + 1, max);
         chars[current] = end;
     }
     return max;
 };
-
-console.log(lengthOfLongestSubstring("abcabcbb"));
-// console.log(lengthOfLongestSubstring("bbbbb"));
-// console.log(lengthOfLongestSubstring("pwwkew"));
