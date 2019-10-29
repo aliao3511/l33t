e186608fcffb17adbdecc131c47ae2abe013bc7d
Alex Liao <alexandraliao3511@gmail.com>
2019-10-29 19:35:20 -0400

    complete lc3 py

---
+++ 3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = 0
+        longest = 0
+        chars = {}
+        for end, ch in enumerate(s):
+            if ch in chars:
+                start = max(chars[ch] + 1, start)
+            longest = max(end - start + 1, longest)
+            chars[ch] = end
+        return longest
