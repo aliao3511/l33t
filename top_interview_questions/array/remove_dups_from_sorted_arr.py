1bc6478b753ea10100028ee930b6d27c9f92df00
Alex Liao <alexandraliao3511@gmail.com>
2020-01-08 09:36:56 -0500

    refactor remove_dups

--- top_interview_questions/array/remove_dups_from_sorted_arr.py
+++ top_interview_questions/array/remove_dups_from_sorted_arr.py
@@ -1,13 +1,21 @@
 class Solution:
   def removeDuplicates(self, nums: List[int]) -> int:  
+    # uniq = 0
+    # idx = 1
+    # current = None
+    # while idx < len(nums):
+      # if nums[idx] == current:
+        # del nums[idx]
+        # uniq += 1
+      # else:
+        # current = nums[idx]
+        # idx += 1
+    # return nums
+    if len(num) == 0: return 0
     uniq = 0
-    idx = 1
-    current = None
-    while idx < len(nums):
-      if nums[idx] == current:
-        del nums[idx]
-        uniq += 1
-      else:
-        current = nums[idx]
-        idx += 1
-    return nums
+    for idx in range(1, len(nums)):
+        if (nums[idx] != nums[uniq]):
+            uniq += 1
+            nums[uniq] = nums[idx]
+    return uniq + 1
+
