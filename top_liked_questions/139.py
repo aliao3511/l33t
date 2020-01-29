d95fe9e8e506de5c01a4e8633f46465e28457801
Alex Liao <alexandraliao3511@gmail.com>
2020-01-29 16:17:25 -0500

    add word break

---
+++ top_liked_questions/139.py
@@ -0,0 +1,22 @@
+def wordBreak(s, wordDict):
+    word_set = set(wordDict)
+    seen = set()
+    queue = [0]
+    while len(queue) > 0:
+        start = queue.pop()
+        if start in seen:
+            continue;
+        seen.add(start)
+        idx = start + 1
+        while idx <= len(s):
+            substring = s[start:idx]
+            if substring in word_set:
+                print(queue)
+                print(substring)
+                if idx == len(s): return True
+                queue.insert(0, idx)
+            idx += 1
+    return False
+
+# print(wordBreak('leetcode', ['leet','code']))
+# print(wordBreak('catsandog', ['cats','dog','sand','and','cat']))
